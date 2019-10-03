d59a8005a0900347fe8ac2b7b47561359e64fd4c
stvoutsin <steliosvoutsinas@yahoo.com>
2019-10-03 14:34:31 +0300

    Updating with Notes on Spark & Kafka Experiments

---
+++ slack/slack_direct.py
@@ -0,0 +1,10 @@
+from slackclient import SlackClient
+
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
+
+sc.api_call(
+  "chat.postMessage",
+  channel="@username",
+  text="Test Message"
+)
